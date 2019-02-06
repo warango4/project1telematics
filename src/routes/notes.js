2e4eb4014a59d9172a6330fd676fc0dfc693836c
warango4 <warango4@eafit.edu.co>
2019-02-05 20:12:39 -0500

    Righting project structure and deleting comments from code

--- src/routes/notes.js
+++
@@ -1,7 +0,0 @@
-const router = require('express').Router();
-
-router.get('/notes', (reg, res) => {
-  res.send('Notes on db');
-});
-
-module.exports = router;
--- src/src/database.js
+++
@@ -1,9 +0,0 @@
-const mongo = require('mongoose');
-
-mongo.connect('mongodb://localhost/geoloc-db-app', {
-    useCreateIndex: true, 
-    useNewUrlParser: true,
-    useFindAndModify: false
-})
-.then(db => console.log('DB is connected'))
-.catch(err => console.error(err));
